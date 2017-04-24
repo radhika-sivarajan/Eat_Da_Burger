883d6e0f35616e0ce0cd58ff115313040ff3443b
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-04-23 20:08:27 -0700

    CSS styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('The Salmon Rushdie', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Salmon Rushdie', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('The Sweet Sunrise', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Barbecue burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('California burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Rice burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rice burger', false);
