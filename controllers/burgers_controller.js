81a72c96d621394358516fe055778392810590f4
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-04-23 18:54:19 -0700

    Update and add new burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,13 +4,29 @@ var burger = require("../models/burger.js");
 var express = require("express");
 var router = express.Router();
 
+// Get list of burgers and display on page
 router.get("/", function(req, res) {
     burger.all(function(data) {
         var burgerObject = {
             burgers: data
         };
-        console.log(burgerObject);
         res.render("index", burgerObject);
+    });
+});
+
+//Devour a burger
+router.put("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.update({ devoured: true }, condition, function() {
+        res.redirect("/");
+    });
+});
+
+//Add a new burger
+router.post("/", function(req, res) {
+    burger.insert(["burger_name"], [req.body.burgerName], function() {
+        res.redirect("/");
     });
 });
 
