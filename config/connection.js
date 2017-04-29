076eb64293c22116f4234ebb8408e05a4fabb88e
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-04-28 18:36:50 -0700

    Heroku database connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // MYSQL connection set up, create and exporting
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// });
+
+var connection = {
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    user: 'b9ecb81df6a0a5',
+    password: '0abf8695',
+    database: 'heroku_d34adf4f865feb9',
+    connectionLimit: 5
+}
 
 connection.connect(function(err) {
     if (err) {
--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,10 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers (
-    id INT NOT NULL AUTO INCREMENT,
-    burger_name VARCHAR(20) NOT NULL,
-    devoured: BOOLEAN DEFAULT false,
-    date TIMESTAMP,
-    PRIMARY KEY (id)
+CREATE TABLE `burgers` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `burger_name` VARCHAR(255) NOT NULL,
+    `devoured` BOOLEAN DEFAULT FALSE,
+    `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
 );
